9e89e27dcd04e25137401601a048af8d8127f60e
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-09-12 21:08:37 +0900

    feat: add sample (Voisonatalk)

---
+++ sample/csharp/VoiSonaTalkLab/Program.cs
@@ -0,0 +1,42 @@
+﻿using LibSasara.Model;
+using LibSasara.VoiSona;
+using LibSasara.VoiSona.Model.Talk;
+
+ConsoleApp.Run<Label>(args);
+
+public class Label: ConsoleAppBase
+{
+	[RootCommand]
+	public async ValueTask<int> ExportAsync(
+		[Option("t", "path to a target tstprj file")]
+		string pathToPrj,
+		[Option("d", "path to export lab files")]
+		string? destination = ""
+	)
+	{
+		if(!Path.Exists(pathToPrj)){
+			return 1;
+		}
+		if(!Directory.Exists(destination)){
+			destination = Path.GetDirectoryName(pathToPrj);
+			if (string.IsNullOrEmpty(destination)) return 1;
+		}
+
+
+		var tstprj = await LibVoiSona
+			.LoadAsync<TstPrj>(pathToPrj);
+		var labs = tstprj
+			.GetAllTracks()
+			.SelectMany(v => v.Utterances)
+			.Where(v => v.Text != "")
+			.Select(v => (v.Text, v.Label))
+			;
+		foreach(var lab in labs){
+			var name = $"{destination}{Path.DirectorySeparatorChar}{lab.Text}.lab";
+			string content = lab.Label!.ToString();
+			await File
+				.WriteAllTextAsync(name, content);
+		}
+		return 0;
+	}
+}
---
+++ sample/csharp/VoiSonaTalkLab/readme.md
@@ -0,0 +1,28 @@
+# Sample; VoiSonaTalkLab
+
+timing label files (`*.lab`) exporter from VoiSona Talk project file (`*.tstprj`).
+
+## Requirements
+
+- Windows x64 (Win 7.0 >=)
+- macOS x64 (macOS 10.12 Sierra >=)
+- macOS arm64 (macOS 10.15 "Catalina" >=)
+- Linux x64
+
+*Only Windows has been tested.
+
+## Download
+
+[Releases](https://github.com/InuInu2022/LibSasara/releases)
+
+## Usage
+
+```cmd
+//windows
+VoiSonaTalkLab.exe -t path\to\yourawesomeproject.tstprj -d pato\to\exportlabfiles\
+
+//mac, linux
+VoiSonaTalkLab -t path/to/yourawesomeproject.tstprj -d pato/to/exportlabfiles/
+```
+
+Please check `--help` command.
