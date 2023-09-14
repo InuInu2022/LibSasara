8a4838ab6b5072a530fad4b3a3ffffc244eac7e7
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-09-14 12:23:10 +0900

    docs: add comments

--- LibSasara.VoiSona/Model/Common.cs
+++ LibSasara.VoiSona/Model/Common.cs
@@ -3,37 +3,37 @@ using System;
 namespace LibSasara.VoiSona.Model;
 
 /// <summary>
-///
+/// common
 /// </summary>
 public static class Common{
-    /// <summary>
-    /// null終端区切り
-    /// </summary>
-    public const byte NULL_END = 0x00;
+	/// <summary>
+	/// null終端区切り
+	/// </summary>
+	public const byte NULL_END = 0x00;
 }
 
 /// <summary>
-///
+/// Type definitions in tssprj/tstprj format
 /// </summary>
 public enum VoiSonaValueType: byte{
-    /// <summary>
-    ///
-    /// </summary>
-    Int32 = 01,
-    /// <summary>
-    ///
-    /// </summary>
-    Bool = 02,
-    /// <summary>
-    ///
-    /// </summary>
-    Double = 04,
-    /// <summary>
-    ///
-    /// </summary>
-    String = 05,
-    /// <summary>
-    ///
-    /// </summary>
-    Unknown = 99,
+	/// <summary>
+	/// int32
+	/// </summary>
+	Int32 = 01,
+	/// <summary>
+	/// boolean
+	/// </summary>
+	Bool = 02,
+	/// <summary>
+	/// double float
+	/// </summary>
+	Double = 04,
+	/// <summary>
+	/// UTF-8 strings
+	/// </summary>
+	String = 05,
+	/// <summary>
+	/// unkown type
+	/// </summary>
+	Unknown = 99,
 }
