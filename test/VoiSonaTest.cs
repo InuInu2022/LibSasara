31bd34fb3f554cc45d805fa6d9ab0a228e32c957
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-10-11 17:57:57 +0900

    test

--- test/VoiSonaTest.cs
+++ test/VoiSonaTest.cs
@@ -472,7 +472,7 @@ public class VoiSonaTest : IAsyncLifetime
 				PhonemeOriginalDuration = "0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9",
 
 				//pitch write
-				RawFrameLogF0 = "0.05:4.4,1.05:4.4"
+				RawFrameLogF0 = "0.05:4.4,0.10:4.4"
 			}
 		});
 
