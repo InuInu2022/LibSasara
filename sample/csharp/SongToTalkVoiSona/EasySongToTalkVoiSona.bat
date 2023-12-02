f7c9773813810eace78eadfa25d26fda71e0f48f
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-12-03 01:52:32 +0900

    feat: add bat file

---
+++ sample/csharp/SongToTalkVoiSona/EasySongToTalkVoiSona.bat
@@ -0,0 +1,19 @@
+@rem 簡単につかえるようにするバッチファイル
+@rem ファイルの中身を書き換えてダブルクリックするだけで使えます
+
+echo "SongToTalkVoiSona VoiSona Talkを歌わせるルールです"
+echo "EasySongToTalkVoiSona.batの中身を書き換えて使ってください"
+
+@rem 【書き換えてOK】ソングトラックのあるccs/ccstへのパス
+@set SRC="./file/kaeru.ccs"
+@rem 【書き換えてOK】上書きするccsへのパス
+@set DIST="./file/dist.ccs"
+@rem 【書き換えてOK】歌わせるキャラ名。持ってるキャラ名にしてね。English name is OK!
+@set CAST="田中傘"
+@rem 【書き換えてOK】感情比率（キャストごとに要変更）
+@set EMOTIONS="[1.0,0.0,0.0,0.0]"
+
+call SongToTalkVoiSona.exe -s %SRC% -d %DIST% -c %CAST% -er %EMOTIONS%
+
+echo "SongToTalk 生成が終了しました！"
+pause
