8e5e62550dcf2f1c3e648753021e79b4bf227373
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-12-07 11:56:42 +0900

    fix: export extesion

--- sample/csharp/SongToTalkVoiSona/EasySongToTalkVoiSona.bat
+++ sample/csharp/SongToTalkVoiSona/EasySongToTalkVoiSona.bat
@@ -7,7 +7,7 @@ echo "EasySongToTalkVoiSona.batの中身を書き換えて使ってください"
 @rem 【書き換えてOK】ソングトラックのあるccs/ccstへのパス
 @set SRC="./file/kaeru.ccs"
 @rem 【書き換えてOK】上書きするttsprjへのパス
-@set DIST="./file/dist.ttsprj"
+@set DIST="./file/dist.tstprj"
 @rem 【書き換えてOK】歌わせるキャラ名。持ってるキャラ名にしてね。English name is OK!
 @set CAST="田中傘"
 @rem 【書き換えてOK】感情比率（キャストごとに要変更）
