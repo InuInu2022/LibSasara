a4615adc2407488b3fe3690418fa721828b7aa3e
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-12-15 03:19:04 +0900

    refactor: remove unused using

--- sample/csharp/SongToTalkVoiSona/Program.cs
+++ sample/csharp/SongToTalkVoiSona/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Concurrent;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
