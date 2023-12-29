1a803ac81a85b2f5e03a6ca4c8db360747059f2f
InuInu2022 <97830071+InuInu2022@users.noreply.github.com>
2023-12-29 15:34:47 +0900

    feat: EasySongToTalkVoiSona.bat  support co option

--- sample/csharp/SongToTalkVoiSona/EasySongToTalkVoiSona.bat
+++ sample/csharp/SongToTalkVoiSona/EasySongToTalkVoiSona.bat
@@ -12,8 +12,10 @@ echo "EasySongToTalkVoiSona.batの中身を書き換えて使ってください"
 @set CAST="田中傘"
 @rem 【書き換えてOK】感情比率（キャストごとに要変更）
 @set EMOTIONS="[1.0,0.0,0.0,0.0]"
+@rem 【書き換えてOK】子音部分のオフセット秒。子音部分をノートの開始時間から前にオフセットします。
+@set OFFSET="0.075"
 
-call SongToTalkVoiSona.exe -s %SRC% -e %DIST% -c %CAST% -er %EMOTIONS%
+call SongToTalkVoiSona.exe -s %SRC% -e %DIST% -c %CAST% -er %EMOTIONS% -co %OFFSET%
 
 echo "SongToTalk 生成が終了しました！"
 pause
